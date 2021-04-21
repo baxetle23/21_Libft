43b317aa50b0382e5653e0b8505fbe7d3ef2a8ee
Lonna Cash <lcash@ge-k4.kzn.21-school.ru>
2021-04-21 15:39:08 +0300

    minus useful fun

--- Libft/ft_strdel.c
+++
@@ -1,6 +0,0 @@
-#include "libft.h"
-
-void    ft_strdel(char **ap) {
-    void **tmp = (void **)ap;
-    ft_memdel(tmp);
-}
--- Libft/ft_strnew.c
+++
@@ -1,5 +0,0 @@
-#include "libft.h"
-
-char *ft_strnew(size_t size) {
-    return (char *)ft_memalloc(size + 1);
-}
